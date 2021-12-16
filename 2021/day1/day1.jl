f4c1a2abe270a54db23b574a66af62b356a43abb
Scott White <scottswhite90@gmail.com>
2021-12-15 22:45:31 -0600

    Completed 2021/day 1 Parts 1 & 2.

--- 2021/day1/day1.jl
+++ 2021/day1/day1.jl
@@ -1,1 +1,27 @@
-array = [1 2 3]
+# Day one of advent of code!
+# Link: https://adventofcode.com/2021/day/1
+
+input = readlines("2021/day1/day1-input.txt")
+
+# Part 1
+
+input_nums = parse.(Int, input)
+
+input_nums[2:end] - input_nums[1:(end - 1)]
+
+count((input_nums[2:end] - input_nums[1:(end - 1)]) .> 0)
+
+
+# Part 2
+
+n = length(input_nums)
+
+sums = []
+
+for i in 1:(n - 2)
+    append!(sums, sum(input_nums[i:(i + 2)]))
+end
+
+sums
+
+count((sums[2:end] - sums[1:(end - 1)]) .> 0)
